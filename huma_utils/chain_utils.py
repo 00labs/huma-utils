28f7d2e90415ab4eb50e334b44e6d2d85531bebd
Jiatu <ljiatu@hotmail.com>
2023-08-18 14:46:22 +0800

    Update chain_utils
    1. Add more chains
    2. Make the chain enum a `StrEnum`
    3. Add chain ID mapping

--- huma_utils/chain_utils.py
+++ huma_utils/chain_utils.py
@@ -1,14 +1,30 @@
+from __future__ import annotations
+
 import enum
 
 
-class Chain(enum.Enum):
-    ETHEREUM = "ETHEREUM"
-    GOERLI = "GOERLI"
-    POLYGON = "POLYGON"
-    MUMBAI = "MUMBAI"
+class Chain(enum.StrEnum):
+    ETHEREUM = enum.auto()
+    GOERLI = enum.auto()
+    SEPOLIA = enum.auto()
+    POLYGON = enum.auto()
+    MUMBAI = enum.auto()
+    CELO = enum.auto()
+    ALFAJORES = enum.auto()
 
     def chain_name(self) -> str:
-        return self.name.lower()
+        return self.lower()
 
     def is_testnet(self) -> bool:
-        return self.chain_name() in ("goerli", "mumbai")
+        return self.chain_name() in ("goerli", "sepolia", "mumbai", "alfajores")
+
+
+CHAIN_ID_BY_NAME = {
+    Chain.ETHEREUM: 1,
+    Chain.GOERLI: 5,
+    Chain.SEPOLIA: 11155111,
+    Chain.POLYGON: 137,
+    Chain.MUMBAI: 80001,
+    Chain.CELO: 42220,
+    Chain.ALFAJORES: 44787,
+}
