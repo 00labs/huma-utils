95d8ee0530ebd3cd0df235af49470b0c162f0b41
Jiatu <ljiatu@hotmail.com>
2023-10-26 14:49:13 +0800

    Update CORD policy
    We need to allow requests from Amplify instead of Netlify.

--- huma_utils/constants.py
+++ huma_utils/constants.py
@@ -1,8 +1,10 @@
 HUMA_FINANCE_DOMAIN_NAME = "huma.finance"
 SERVER_ALLOW_ORIGIN_REGEX = (
-    r"^https://deploy-preview-\d+--dev-app-huma\.netlify\.app"
+    r"^https://pr-\d+\.d382yqc38xh8lk\.amplifyapp\.com"
+    r"|https://master\.d382yqc38xh8lk\.amplifyapp\.com"
     r"|https://dev\.app\.huma\.finance"
     r"|https://app\.huma\.finance"
+    r"|https://v2\.app\.huma\.finance"
     r"|https://local\.bulla\.network:1234"
     r"|https://dev\.bulla\.network"
     r"|https://banker\.bulla\.network"
