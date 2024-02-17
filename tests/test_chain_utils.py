935b2ecfe6ad580f8470944e0d42fc08e80bc409
Jiatu <ljiatu@hotmail.com>
2024-02-17 00:22:07 -0800

    Add `chain_from_id` function

--- tests/test_chain_utils.py
+++ tests/test_chain_utils.py
@@ -27,3 +27,14 @@ def describe_Chain() -> None:
 def describe_CHAIN_ID_BY_NAME() -> None:
     def it_contains_the_id_for_all_chains() -> None:
         assert len(chain_utils.CHAIN_ID_BY_NAME) == len(chain_utils.Chain)
+
+
+def describe_chain_from_id() -> None:
+    def if_the_chain_is_supported() -> None:
+        def it_returns_the_chain() -> None:
+            assert chain_utils.chain_from_id(1) == chain_utils.Chain.ETHEREUM
+
+    def if_the_chain_is_not_supported() -> None:
+        def it_raises_exception() -> None:
+            with pytest.raises(chain_utils.UnsupportedChainException):
+                chain_utils.chain_from_id(0)
