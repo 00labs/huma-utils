1b100a9cda2bfeb68b032f79f724b2d1a849ef62
Jiatu <ljiatu@hotmail.com>
2024-10-08 20:06:42 +0800

    Bump package versions
    Bump package versions. The most important ones are Pydantic and web3.py, which had breaking changes.

--- tests/test_web3_utils.py
+++ tests/test_web3_utils.py
@@ -1,12 +1,11 @@
-import pydantic
+import pydantic_settings
 import pytest
 
 from huma_utils import chain_utils, web3_utils
 
 
-class Settings(pydantic.BaseSettings):
-    class Config:
-        case_sensitive = False
+class Settings(pydantic_settings.BaseSettings):
+    model_config = pydantic_settings.SettingsConfigDict(case_sensitive=False)
 
     chain: chain_utils.Chain
     web3_provider_url: str
@@ -19,17 +18,15 @@ def describe_get_w3() -> None:
     async def it_can_get_w3() -> None:
         w3 = await web3_utils.get_w3(settings.chain, settings.web3_provider_url)
         assert w3 is not None
-        is_connected = await w3.is_connected()  # type: ignore[misc]
+        is_connected = await w3.is_connected()
         assert is_connected is True
 
-        # The type annotation for `eth` is wrong: it's annotated as `eth`, but it should be `AsyncEth`
-        # in this case. If the type annotation is correct then it wouldn't complain about the presence of `await`.
-        latest_block = await w3.eth.get_block("latest")  # type: ignore[misc]
+        latest_block = await w3.eth.get_block("latest")
         assert latest_block is not None
         assert latest_block["number"] > 0
         assert latest_block["hash"] is not None
 
-        block = await w3.eth.get_block(latest_block["hash"])  # type: ignore[misc]
+        block = await w3.eth.get_block(latest_block["hash"])
         assert block is not None
         assert block["number"] == latest_block["number"]
 
